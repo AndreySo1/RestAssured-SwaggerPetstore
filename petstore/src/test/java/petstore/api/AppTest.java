cc32e9533f3bbf9c556de6e6554e4ba19d1e8778
AndreySo1 <andrey.kafka@gmail.com>
2022-09-12 16:06:43 +0300

    init: start

---
+++ petstore/src/test/java/petstore/api/AppTest.java
@@ -0,0 +1,14 @@
+package petstore.api;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertTrue(true);
+    }
+}
